e98255e509e3b9a6b158c4dd8f731a47e569c59c
jpavelec <jospavelec@gmail.com>
2017-03-29 00:51:41 +0200

    Click on CardView

---
+++ Porenut/app/src/main/java/cz/muni/fi/pv239/porenut/Category.java
@@ -0,0 +1,14 @@
+package cz.muni.fi.pv239.porenut;
+
+/**
+ * Created by Josef Pavelec on 29/03/17.
+ */
+
+public class Category {
+
+    public String title;
+
+    public Category(String title) {
+        this.title = title;
+    }
+}
